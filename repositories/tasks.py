3103b0c1fb16c618b3a186f7a4d47432e3c1ee8c
danrleypereira <danrleywillian@gmail.com>
2021-08-19 05:36:16 -0300

    adding last 30 days commits to database

--- repositories/tasks.py
+++ repositories/tasks.py
@@ -1,12 +1,15 @@
 from os import access
+from repositories import serializers
 import requests as req
 import json
 from datetime import datetime, timedelta
 from githubmonitor.celery import app
 from social_django.models import UserSocialAuth
-# from django.contrib.auth.models import User
 
-from .models import Repository
+from celery.utils.log import get_task_logger
+from .models import Repository, Commit
+
+logger = get_task_logger(__name__)
 
 @app.task(serializer='json')
 def check_repo_exists_remote(ui_request):
@@ -16,8 +19,10 @@ def check_repo_exists_remote(ui_request):
     url = url_base+ str(owner) +"/"+ str(repo)
     request_repo_git = req.get(url)
     if request_repo_git.status_code != 200:
+        logger.info("Repository does not exist at remote: {0}".format(str(repo)))
         return False
-    return True 
+    logger.info("Repository exist at remote: {0}".format(str(repo)))
+    return True
 
 @app.task(serializer='json')
 def check_repo_exists_database(ui_request):
@@ -28,16 +33,36 @@ def check_repo_exists_database(ui_request):
         return False
     return True
 
-@app.task(serializer='json')
-def get_last30days_commits(ui_request):
-    repo = ui_request.data["name"]
+@app.task(bind=True, retry_limit=4, default_retry_delay=10, serializer='json')
+def save_last30days_commits(self, user, repo):
     try:
-        repo = Repository.objects.get(name=str(repo))
-    except Repository.DoesNotExist:
-        return
+        repo = Repository.objects.get(name=repo)
+    except Repository.DoesNotExist as e:
+        logger.info('Repository does not exist: {0}'.format(repo))
+        self.retry(exc= e)
+    commits = get_last30days_commits(str(repo), user)
+
+    logger.info('Commits from remote: {0}'.format(commits))
+    try:
+        for com in commits:
+            serializer = Commit(
+                message= com["commit"]["message"],
+                sha= com["sha"],
+                author= com["commit"]["author"]["name"],
+                url= com["url"],
+                date= com["commit"]["author"]["date"],
+                avatar= com["commit"]["author"]["name"],
+                repository= repo
+            )
+            serializer.save()
+    except Exception as e:
+        self.retry(exc= e)
+
+
+@app.task(serializer='json')
+def get_last30days_commits(repo, owner):
     url_base = "https://api.github.com/repos/"
-    owner = ui_request.user
-    url = url_base + str(owner) +"/"+ str(repo.name) +"/commits"
+    url = url_base + str(owner) +"/"+ repo +"/commits"
     today = datetime.today()
     payload = UserSocialAuth.objects.select_related("user").get(user_id=owner.id)
     access_token = payload.__dict__["extra_data"]["access_token"]
@@ -46,9 +71,9 @@ def get_last30days_commits(ui_request):
     response_commits = req.get(url, params={ "since": today - timedelta(days=31)}, headers={
         "Authorization": token_type +" "+ access_token,
     })
-    res = response_commits._content.decode("ascii")
-    logging(res)
+    res = response_commits._content.decode("utf8")
     return json.loads(res)
+
 
 @app.task(serializer='json')
 def request_user_data(ui_request):
--- repositories/views.py
+++ repositories/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import Commit, Repository
+from .models import Commit
 from .serializers import CommitSerializer, RepositorySerializer
 from .tasks import *
 
@@ -17,13 +17,13 @@ def commit_list_view(request):
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
-def repository_create_view(request):  
+def repository_create_view(request):
     validate = check_repo_exists_remote(request) and not check_repo_exists_database(request)
     if(validate):
         serializer = RepositorySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        get_last30days_commits(request)
+        save_last30days_commits(request.user, request.data["name"])
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response({"message":"Not Found or Already on database."}, status=status.HTTP_404_NOT_FOUND)
