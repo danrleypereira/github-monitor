b940e44640f96f872437b7708122de0b6881b585
danrleypereira <danrleywillian@gmail.com>
2021-08-18 23:57:23 -0300

    creating tasks to verify if repository exists on remote or db

--- repositories/tasks.py
+++ repositories/tasks.py
@@ -1,2 +1,47 @@
-# from githubmonitor.celery import app
-# Uncomment and create your tasks here.
+from os import access
+import requests as req
+import json
+from githubmonitor.celery import app
+from social_django.models import UserSocialAuth
+from django.contrib.auth.models import User
+
+from .models import Repository
+
+@app.task(serializer='json')
+def check_repo_exists_remote(ui_request):
+    url_base = "https://api.github.com/repos/"
+    owner = ui_request.user
+    repo = ui_request.data["name"]
+    url = url_base+ str(owner) +"/"+ str(repo)
+    request_repo_git = req.get(url)
+    if request_repo_git.status_code != 200:
+        return False
+    return True 
+
+@app.task(serializer='json')
+def check_repo_exists_database(ui_request):
+    repo = ui_request.data["name"]
+    try:
+        Repository.objects.get(name=str(repo))
+    except Repository.DoesNotExist:
+        return False
+    return True
+
+@app.task(serializer='json')
+def request_user_data(ui_request):
+    url_base = "https://api.github.com/user"
+    owner = ui_request.user
+    url = url_base
+    payload = UserSocialAuth.objects.select_related("user").get(user_id=owner.id)
+    access_token = payload.__dict__["extra_data"]["access_token"]
+    token_type = payload.__dict__["extra_data"]["token_type"].capitalize()
+    response_user = req.get(url, params=None, headers={
+        "Authorization": token_type +" "+ access_token,
+    })
+    res = response_user._content.decode("ascii")
+    return json.loads(res)
+
+@app.task(serializer='json')
+def logging(msg):
+    log = msg
+    print("\n"+ len(str(log))*"=" + "\n" + str(log) + "\n"+ len(str(log))*"=" + "\n")
--- repositories/views.py
+++ repositories/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from .models import Commit
 from .serializers import CommitSerializer, RepositorySerializer
-
+from .tasks import *
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -17,8 +17,13 @@ def commit_list_view(request):
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
-def repository_create_view(request):
-    serializer = RepositorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+def repository_create_view(request):  
+    validate = check_repo_exists_remote(request) and not check_repo_exists_database(request)
+    if(validate):
+        serializer = RepositorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # Implement task to retrieve all commit (last 30 days) here.
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response({"message":"Not Found or Already on database."}, status=status.HTTP_404_NOT_FOUND)
