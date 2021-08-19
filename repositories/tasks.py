161a80d71a65290aed25663ae9bb7789e8b00df4
danrleypereira <danrleywillian@gmail.com>
2021-08-19 00:42:34 -0300

    getting the last 30 days commits from repo using github api

--- repositories/tasks.py
+++ repositories/tasks.py
@@ -1,9 +1,10 @@
 from os import access
 import requests as req
 import json
+from datetime import datetime, timedelta
 from githubmonitor.celery import app
 from social_django.models import UserSocialAuth
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 from .models import Repository
 
@@ -26,6 +27,28 @@ def check_repo_exists_database(ui_request):
     except Repository.DoesNotExist:
         return False
     return True
+
+@app.task(serializer='json')
+def get_last30days_commits(ui_request):
+    repo = ui_request.data["name"]
+    try:
+        repo = Repository.objects.get(name=str(repo))
+    except Repository.DoesNotExist:
+        return
+    url_base = "https://api.github.com/repos/"
+    owner = ui_request.user
+    url = url_base + str(owner) +"/"+ str(repo.name) +"/commits"
+    today = datetime.today()
+    payload = UserSocialAuth.objects.select_related("user").get(user_id=owner.id)
+    access_token = payload.__dict__["extra_data"]["access_token"]
+    token_type = payload.__dict__["extra_data"]["token_type"].capitalize()
+
+    response_commits = req.get(url, params={ "since": today - timedelta(days=31)}, headers={
+        "Authorization": token_type +" "+ access_token,
+    })
+    res = response_commits._content.decode("ascii")
+    logging(res)
+    return json.loads(res)
 
 @app.task(serializer='json')
 def request_user_data(ui_request):
--- repositories/views.py
+++ repositories/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import Commit
+from .models import Commit, Repository
 from .serializers import CommitSerializer, RepositorySerializer
 from .tasks import *
 
@@ -23,7 +23,7 @@ def repository_create_view(request):
         serializer = RepositorySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        # Implement task to retrieve all commit (last 30 days) here.
+        get_last30days_commits(request)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response({"message":"Not Found or Already on database."}, status=status.HTTP_404_NOT_FOUND)
