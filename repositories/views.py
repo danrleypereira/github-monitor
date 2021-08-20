ccca8675c623ef3fc8ba886c16d56eb1c1514ab9
danrleypereira <danrleywillian@gmail.com>
2021-08-20 01:48:15 -0300

    code formatted using pep8 style guide

--- repositories/views.py
+++ repositories/views.py
@@ -1,14 +1,13 @@
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from rest_framework.views import APIView
 
 from .models import Commit
 from .serializers import CommitSerializer, RepositorySerializer
+from .services import check_repo_exists_database, check_repo_exists_remote
 from .tasks import save_last30days_commits
 
-from .services import check_repo_exists_remote, check_repo_exists_database
 
 class CommitView(APIView):
     permission_classes = [IsAuthenticated]
@@ -18,16 +17,19 @@ class CommitView(APIView):
         serializer = CommitSerializer(commits, many=True)
         return Response(serializer.data)
 
+
 class RepositoryView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
-        validate = check_repo_exists_remote(request) and not check_repo_exists_database(request)
+        validate = check_repo_exists_remote(
+            request) and not check_repo_exists_database(request)
         if(validate):
             serializer = RepositorySerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            save_last30days_commits.delay(request.data["name"], request.user.id)
+            save_last30days_commits.delay(
+                request.data['name'], request.user.id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"message":"Not Found or Already on database."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Not Found or Already on database.'}, status=status.HTTP_404_NOT_FOUND)
